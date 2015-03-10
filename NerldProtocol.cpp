54a2edd323f41bce0ce4b5ec07f6939c70b69abe
Khanh Nguyen <khanh@intersect.org.au>
2015-03-10 18:45:27 +1100

    Add NerldProtocol

---
+++ NerldProtocol.cpp
@@ -0,0 +1,124 @@
+#include "NerldProtocol.h"
+
+NerldProtocol::NerldProtocol() : m_port(NULL) {}
+
+NerldProtocol::NerldProtocol(HardwareSerial *port) : m_port(port) {}
+
+int NerldProtocol::requestAddresssFromMaster() {
+	int address = NULL;
+	Wire.requestFrom(1,1);
+	while (address == NULL && Wire.available()) {
+    address = (Wire.read());
+  }
+  return address;
+}
+
+int NerldProtocol::sendCommandToMaster(int slaveAddress, int command, int val) {
+	char encMessage[128];
+	if (encodeMessage(slaveAddress, command, val, encMessage) == 0) {
+		wireTransmit(encMessage);
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int NerldProtocol::sendCommandToMaster(int slaveAddress, int command, float val) {
+	char encMessage[128];
+	if (encodeMessage(slaveAddress, command, val, encMessage) == 0) {
+		wireTransmit(encMessage);
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int NerldProtocol::requestFromSlave(int slaveAddress) {
+	Wire.requestFrom(slaveAddress,1);
+	int retVal = NULL;
+  while(retVal == NULL && Wire.available()) {
+  	retVal = Wire.read();
+  }
+	return retVal;
+}
+
+int NerldProtocol::sendToSlave(int slaveAddress, int command, int val) {
+	char encMessage[128];
+	if (encodeMessage(slaveAddress, command, val, encMessage) == 0) {
+		wireTransmit(encMessage);
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int NerldProtocol::sendToSlave(int slaveAddress, int command, float val) {
+	char encMessage[128];
+	if (encodeMessage(slaveAddress, command, val, encMessage) == 0) {
+		wireTransmit(encMessage);
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int NerldProtocol::sendToBridge(int address, int command, int val) {
+	if (m_port == NULL) {
+		return 1;
+	}
+	char encMessage[128];
+	if (encodeMessage(address, command, val, encMessage) == 0) {
+		m_port->print(encMessage);
+
+		// parse and return the response from the bridge.
+
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int NerldProtocol::receiveCommand(int numOfBytes, String * output) {
+	for (int j = 0; j < 3; j++) {
+		output[j] = "";
+	}
+
+	int i = 0;
+	int currentArgIdx = 0;
+  while(Wire.available() && i < numOfBytes)
+  {
+    char c = Wire.read();
+    
+    if (c != ':') {
+    	output[currentArgIdx] += c;
+    } else {
+    	currentArgIdx++;
+    }
+    i++;
+  }
+
+  return 0;
+}
+
+void NerldProtocol::wireTransmit(char * buffer) {
+	Wire.beginTransmission(1);
+	Wire.write(buffer);
+	Wire.endTransmission();
+}
+
+int NerldProtocol::encodeMessage(int address, int command, int value, char * output) {
+	String message = "";
+	message = message + address + ":" + command + ":" + value;
+	message.toCharArray(output, 128);
+	return 0;
+}
+
+int NerldProtocol::encodeMessage(int address, int command, float value, char * output) {
+	char floatBuffer[10];
+	dtostrf(value, 5, 3, floatBuffer);
+
+	String message = "";
+	message = message + address + ":" + command + ":" + floatBuffer;
+	message.toCharArray(output, 128);
+	return 0;
+}
---
+++ NerldProtocol.h
@@ -0,0 +1,44 @@
+#ifndef NERLD_PROTOCOL_H
+#define NERLD_PROTOCOL_H
+
+#include <Wire.h>
+#include <HardwareSerial.h>
+
+class NerldProtocol {
+public:
+
+	// Constructor
+	NerldProtocol();
+	NerldProtocol(HardwareSerial *port);
+
+	// Send to Master Module Communication
+	int requestAddresssFromMaster();
+	int sendCommandToMaster(int slaveAddress, int command, int val);
+	int sendCommandToMaster(int slaveAddress, int command, float val);
+
+	// Send to Slave Module Communication
+	int requestFromSlave(int slaveAddress);
+	int sendToSlave(int slaveAddress, int command, int val);
+	int sendToSlave(int slaveAddress, int command, float val);
+
+	// Send to Bridge (from Master only)
+	int sendToBridge(int address, int command, int val);
+
+	// Message parsing
+	int receiveCommand(int numOfBytes, String * ouput);
+
+	HardwareSerial * m_port;
+
+private:
+	// Wire Functions
+	void wireTransmit(char * buffer);
+
+	// Encode Message
+	int encodeMessage(int address, int command, int value, char * output);
+	int encodeMessage(int address, int command, float value, char * output);
+
+	// Decode Message
+};
+
+
+#endif
